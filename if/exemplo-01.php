377c63e208415f2107214cafd18ada930ec23912
Paulo Vitor <vitorpaulo17@hotmail.com>
2019-02-24 02:17:03 -0300

    codificando em php if

---
+++ if/exemplo-01.php
@@ -0,0 +1,21 @@
+<?php
+$qualSuaIdade= 30;
+
+$idadeCrianca= 12;
+$idadeMaior = 18;
+$idadeMelhor = 65;
+
+if($qualSuaIdade < $idadeCrianca){
+
+echo "criança";
+
+}else if($qualSuaIdade < $idadeMaior){
+
+echo "adolecente";
+}else{
+
+	echo "Não é criança !!!";
+}
+
+
+?>
