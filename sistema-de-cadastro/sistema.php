c1585f4cdddb6d4d5911443ad7cd546d73abe5d5
Paulo Vitor <vitorpaulo17@hotmail.com>
2021-11-24 17:47:59 -0300

    logo da pagina e css separado da tela do login

--- sistema-de-cadastro/sistema.php
+++ sistema-de-cadastro/sistema.php
@@ -17,6 +17,7 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="shortcut icon" type="imagex/png" href="../sistema-cadastro-php/Imagens/dafonteLogo.ico">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <title>SISTEMA | PV</title>
